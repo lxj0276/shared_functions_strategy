d7b0ea42ecaf2ae58b655b8701b28fb16d439650
Jiapeng <wqxl309@hotmail.com>
2016-09-22 13:59:08 +0800

    nochange

---
+++ other_day_indicators.m
@@ -0,0 +1,6 @@
+function[indicators]=other_day_indicators(tableK)
+% tableK is of order date signals positions points returns netval and maybe
+% some indicators following, this function should use only the first 6
+% columns of tableK and generate all other indicators as a table and paste
+% them after tableK
+
